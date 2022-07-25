e26bef2c29172cb9fbe4eb7e1e776d0d8a983785
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2022-07-25 09:01:17 -0500

    working on changes

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,30 +1,24 @@
 import React from "react";
 import { Link, Head } from "@inertiajs/inertia-react";
+import Table from "../Components/Tables/Table";
 
-export default function Welcome(props) {
+export default function Welcome() {
     return (
         <>
             <Head title="U.S. States" />
             <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
                 <div className="fixed top-0 right-0 px-6 py-4 sm:block">
                     <>
-                        <Link
-                            href={route("login")}
-                            className="text-sm text-gray-700 underline"
-                        >
-                            Log in
-                        </Link>
-
-                        <Link
-                            href={route("register")}
-                            className="ml-4 text-sm text-gray-700 underline"
-                        >
-                            Register
-                        </Link>
+                        <label class="switch">
+                            <input type="checkbox" />
+                            <span class="slider round"></span>
+                        </label>
                     </>
                 </div>
 
-                <div className="max-w-6xl mx-auto sm:px-6 lg:px-8"></div>
+                <div className="w-6xl mx-auto sm:px-6 lg:px-8">
+                    <Table />
+                </div>
             </div>
         </>
     );
